a036e455902e2d4814c9bacc12b5e7e399071887
tarantino19 <tolentinored19@gmail.com>
2024-01-11 18:43:54 +0800

    add basic auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
 	return <main className='flex min-h-screen w-full items-center justify-center'>{children}</main>;
+	<b>Hello</b>;
 };
 
 export default Layout;
--- app/(auth)/sign-in/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import page from '../sign-up/page';
-
-const SignIn = () => {
-	return <div>sign-in</div>;
-};
-
-export default SignIn;
