1bf001c86fe8d757e95ef2ec9e3bbd545122e6d0
tarantino19 <tolentinored19@gmail.com>
2024-01-11 14:46:03 +0800

    initial routing setup

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import page from '../sign-up/page';
+
+const SignIn = () => {
+	return <div>sign-in</div>;
+};
+
+export default SignIn;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-	return (
-		<main>
-			<h1 className='h1-bold'>NextJS 14 we&apos;re coming</h1>
-			<h2 className='h2-bold'>NextJS 14 we&apos;re coming</h2>
-			<h3 className='h3-bold'>NextJS 14 we&apos;re coming</h3>
-		</main>
-	);
-}
